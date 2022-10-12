0aa682926dc145b1a9382e89552407e08db3e861
Aldiramadhan271 <ramadhanaldi60@gmail.com>
2022-10-12 20:44:13 +0700

    first commit

---
+++ src/components/Business.jsx
@@ -0,0 +1,43 @@
+import { features } from "../constants"
+import styles, { layout } from "../style"
+import Button from './Button'
+
+const FeaturCard = ({icon, title, content, index}) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+
+  </div>
+)
+
+const business = () => {
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>you do the business, we'll handle the money <br className="sm:block hidden" /></h2>
+        <p className={`${styles.paragraph} max-w[470px] mt-5 `}>with the right credit card, you can improve your financial life by building credit, earning rewards and saving money. but with hundreds of credit cards on the market.</p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeaturCard key={feature.id} {...feature} index />
+        ))}
+      </div>
+
+    </section>
+  )
+}
+
+export default business
